e86a8d3f705c5de3570b96ee35ef670493a8d9e3
KonstantinKhodot <Khodot.konstantin@mail.ru>
2022-10-24 14:37:51 +0300

    Correction2

--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -18,6 +18,32 @@ public class RadioTest {
     }
 
     @Test
+    public void moreThanTheMaximumRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+
+        radio.next();
+
+        int actual = radio.getCurrentRadioStation();
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lessThanTheMinimumRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(0);
+
+        radio.prev();
+
+        int actual = radio.getCurrentRadioStation();
+        int expected = 9;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     public void currentRadioStation() {
         Radio radio = new Radio();
         radio.setCurrentRadioStation(0);
@@ -49,6 +75,30 @@ public class RadioTest {
 
         int actual = radio.getCurrentVolume();
         int expected = 6;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void moreThanTheMaximumVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(10);
+
+        radio.increaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 10;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void lessThanTheMinimumVolume() {
+        Radio radio = new Radio();
+        radio.setCurrentVolume(0);
+
+        radio.decreaseVolume();
+
+        int actual = radio.getCurrentVolume();
+        int expected = 0;
         assertEquals(expected, actual);
     }
 
